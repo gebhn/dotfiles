ec6b8b7f2ad9ba7b5bcb05e5a8e2f2fc3a217170
Nicholas Gebhart <gebhn@proton.me>
2025-05-19 22:59:22 -0700

    Updates configs
    - Adds staticcheck to nvim-confirm
    - Uses better path for foot.ini

--- .config/nvim/lua/plugins/nvim-conform.lua
+++ .config/nvim/lua/plugins/nvim-conform.lua
@@ -7,7 +7,7 @@ mini.later(function()
         notify_on_error = false,
         format_on_save = { lsp_fallback = true, timeout_ms = 500 },
         formatters_by_ft = {
-            go = { 'gofmt', 'goimports' },
+            go = { 'gofmt', 'staticcheck', 'goimports' },
             lua = { 'stylua' },
             zig = { 'zigfmt' },
             ['_'] = { 'trim_whitespace' },
