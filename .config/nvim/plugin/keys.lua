d0c40d1aa13a395136eab291a1f79ef6c3f187c2
Nicholas Gebhart <mail@gebhn.com>
2025-09-02 19:37:31 -0700

    fix: keys

--- .config/nvim/plugin/keys.lua
+++ .config/nvim/plugin/keys.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('i', 'jk', '<ESC>')
 
-vim.keymap.set('c', '<C-y>', '<ESC>')
+vim.keymap.set('c', '<C-y>', '<CR>')
 
 vim.keymap.set('n', 'ff', ':Files ')
 vim.keymap.set('n', 'fs', ':Grep ')
